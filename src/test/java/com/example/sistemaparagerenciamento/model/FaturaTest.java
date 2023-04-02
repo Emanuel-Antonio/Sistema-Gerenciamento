725c1ee6ba0ea78019a78e7c0c10c9f3287b99e5
Emanuel-Antonio <102038148+Emanuel-Antonio@users.noreply.github.com>
2023-04-02 17:15:35 -0300

    refactor: refatoração fatura e criação do teste do metodo novoPagamento

---
+++ src/test/java/com/example/sistemaparagerenciamento/model/FaturaTest.java
@@ -0,0 +1,16 @@
+package com.example.sistemaparagerenciamento.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+class FaturaTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
