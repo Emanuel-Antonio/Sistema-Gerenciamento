e868a1881e5405a3d79f2b4e19d4a341fc0fd810
Emerson-Rodrigo <102042566+Emerson-Rodrigo-Lima-Pereira@users.noreply.github.com>
2023-04-03 23:00:50 -0300

    style: alterações na implementação de Serviço

--- src/main/java/com/example/sistemaparagerenciamento/dao/servico/ServicoDAO.java
+++ src/main/java/com/example/sistemaparagerenciamento/dao/servico/ServicoDAO.java
@@ -1,8 +1,6 @@
 package com.example.sistemaparagerenciamento.dao.servico;
 
-import com.example.sistemaparagerenciamento.dao.CRUD;
 import com.example.sistemaparagerenciamento.model.Peca;
-import com.example.sistemaparagerenciamento.model.Servico;
 
 public interface ServicoDAO {
 
