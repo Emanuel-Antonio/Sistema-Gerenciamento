ef9af55b8ba7479a1bd5a6306bf413be166e0acb
Emanuel-Antonio <102038148+Emanuel-Antonio@users.noreply.github.com>
2023-04-09 12:00:53 -0300

    feat: teste model fatura

--- src/test/java/com/example/sistemaparagerenciamento/model/FaturaTest.java
+++ src/test/java/com/example/sistemaparagerenciamento/model/FaturaTest.java
@@ -1,22 +1,25 @@
 package com.example.sistemaparagerenciamento.model;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class FaturaTest {
 
+    private Fatura fatura;
     @BeforeEach
     void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
+        this.fatura = new Fatura(1);
     }
 
     @Test
     void setValorPago() {
+        this.fatura.setValorPago(10.0);
+        assertEquals( 10.0, this.fatura.getValorPago());
+        this.fatura.setValorPago(20.0);
+        assertEquals(30.0,this.fatura.getValorPago());
     }
+
 }
