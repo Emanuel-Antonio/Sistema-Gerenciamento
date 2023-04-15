f21b260e2418d7b67fe2d43991bf8fb556171399
Emerson-Rodrigo <102042566+Emerson-Rodrigo-Lima-Pereira@users.noreply.github.com>
2023-04-15 11:16:09 -0300

    feat: criando uma exception para CategoriaServicoNaoEncontrada

---
+++ src/main/java/com/example/sistemaparagerenciamento/exceptions/categoriaservico/CategoriaServicoNaoEncontrada.java
@@ -0,0 +1,10 @@
+package com.example.sistemaparagerenciamento.exceptions.categoriaservico;
+
+public class CategoriaServicoNaoEncontrada extends Exception{
+
+
+    public CategoriaServicoNaoEncontrada(String message) {
+        super(message);
+    }
+
+}
