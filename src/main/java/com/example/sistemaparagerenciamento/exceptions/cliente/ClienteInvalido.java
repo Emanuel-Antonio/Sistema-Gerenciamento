7764b2efb88f396e5b2567c17d39a01e52719850
Emerson-Rodrigo <102042566+Emerson-Rodrigo-Lima-Pereira@users.noreply.github.com>
2023-04-15 11:42:43 -0300

    feat: criando uma exception para ClienteInvalido

---
+++ src/main/java/com/example/sistemaparagerenciamento/exceptions/cliente/ClienteInvalido.java
@@ -0,0 +1,9 @@
+package com.example.sistemaparagerenciamento.exceptions.cliente;
+
+public class ClienteInvalido extends Exception{
+
+
+    public ClienteInvalido(String message) {
+        super(message);
+    }
+}
