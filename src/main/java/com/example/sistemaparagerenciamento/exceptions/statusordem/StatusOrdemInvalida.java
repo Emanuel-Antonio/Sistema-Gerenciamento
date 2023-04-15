4f02567edb6688340b1c81afaf93ad68ee02913e
Emerson-Rodrigo <102042566+Emerson-Rodrigo-Lima-Pereira@users.noreply.github.com>
2023-04-15 11:55:51 -0300

    feat: criando uma exception para StatusOrdemInvalida e StatusOrdemNaoEncontrado

---
+++ src/main/java/com/example/sistemaparagerenciamento/exceptions/statusordem/StatusOrdemInvalida.java
@@ -0,0 +1,9 @@
+package com.example.sistemaparagerenciamento.exceptions.statusordem;
+
+public class StatusOrdemInvalida extends Exception{
+
+
+    public StatusOrdemInvalida(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/sistemaparagerenciamento/exceptions/statusordem/StatusOrdemNaoEncontrado.java
@@ -0,0 +1,9 @@
+package com.example.sistemaparagerenciamento.exceptions.statusordem;
+
+public class StatusOrdemNaoEncontrado extends Exception{
+
+
+    public StatusOrdemNaoEncontrado(String message) {
+        super(message);
+    }
+}
