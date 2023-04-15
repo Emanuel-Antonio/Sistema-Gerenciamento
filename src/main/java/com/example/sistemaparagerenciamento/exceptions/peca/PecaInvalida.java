0df32ef0cfeb4f2d71c803404a238db390e8b850
Emerson-Rodrigo <102042566+Emerson-Rodrigo-Lima-Pereira@users.noreply.github.com>
2023-04-15 11:51:25 -0300

    feat: criando uma exception para PecaInvalida e PecaNaoEncontrada

---
+++ src/main/java/com/example/sistemaparagerenciamento/exceptions/peca/PecaInvalida.java
@@ -0,0 +1,9 @@
+package com.example.sistemaparagerenciamento.exceptions.peca;
+
+public class PecaInvalida extends Exception{
+
+
+    public PecaInvalida(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/sistemaparagerenciamento/exceptions/peca/PecaNaoEncontrada.java
@@ -0,0 +1,9 @@
+package com.example.sistemaparagerenciamento.exceptions.peca;
+
+public class PecaNaoEncontrada extends Exception{
+
+
+    public PecaNaoEncontrada(String message) {
+        super(message);
+    }
+}
