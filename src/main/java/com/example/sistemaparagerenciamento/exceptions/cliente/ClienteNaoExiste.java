9e47f14429231f05dd90985713780ebb96d558e0
Emerson-Rodrigo <102042566+Emerson-Rodrigo-Lima-Pereira@users.noreply.github.com>
2023-04-15 11:18:26 -0300

    feat: criando uma exception para ClienteNaoExiste

---
+++ src/main/java/com/example/sistemaparagerenciamento/exceptions/cliente/ClienteNaoExiste.java
@@ -0,0 +1,9 @@
+package com.example.sistemaparagerenciamento.exceptions.cliente;
+
+public class ClienteNaoExiste extends Exception{
+
+
+    public ClienteNaoExiste(String message) {
+        super(message);
+    }
+}
