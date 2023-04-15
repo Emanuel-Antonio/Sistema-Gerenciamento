a305d7cebe01422cfa98bc1ffccb68a65cf7b6a2
Emerson-Rodrigo <102042566+Emerson-Rodrigo-Lima-Pereira@users.noreply.github.com>
2023-04-15 11:21:51 -0300

    feat: criando uma exception para FaturaNaoExiste

---
+++ src/main/java/com/example/sistemaparagerenciamento/exceptions/fatura/FaturaNaoExiste.java
@@ -0,0 +1,9 @@
+package com.example.sistemaparagerenciamento.exceptions.fatura;
+
+public class FaturaNaoExiste extends Exception{
+
+
+    public FaturaNaoExiste(String message) {
+        super(message);
+    }
+}
