d6822a3b1d17cbb28cd0d7623775e2c7112867a2
Emerson-Rodrigo <102042566+Emerson-Rodrigo-Lima-Pereira@users.noreply.github.com>
2023-04-15 11:53:20 -0300

    feat: criando uma exception para ServicoInvalido e ServicoNaoEncontrado

---
+++ src/main/java/com/example/sistemaparagerenciamento/exceptions/servico/ServicoInvalido.java
@@ -0,0 +1,9 @@
+package com.example.sistemaparagerenciamento.exceptions.servico;
+
+public class ServicoInvalido extends Exception{
+
+
+    public ServicoInvalido(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/sistemaparagerenciamento/exceptions/servico/ServicoNaoEncontrado.java
@@ -0,0 +1,9 @@
+package com.example.sistemaparagerenciamento.exceptions.servico;
+
+public class ServicoNaoEncontrado extends Exception{
+
+
+    public ServicoNaoEncontrado(String message) {
+        super(message);
+    }
+}
