5c2655786bc25e9e050c601a21b143d90664c2cb
Emerson-Rodrigo <102042566+Emerson-Rodrigo-Lima-Pereira@users.noreply.github.com>
2023-04-15 11:27:57 -0300

    feat: criando uma exception para OrdemInvalida e OrdemNaoEncontrada

---
+++ src/main/java/com/example/sistemaparagerenciamento/exceptions/ordem/OrdemInvalida.java
@@ -0,0 +1,9 @@
+package com.example.sistemaparagerenciamento.exceptions.ordem;
+
+public class OrdemInvalida extends Exception{
+
+
+    public OrdemInvalida(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/sistemaparagerenciamento/exceptions/ordem/OrdemNaoEncontrada.java
@@ -0,0 +1,9 @@
+package com.example.sistemaparagerenciamento.exceptions.ordem;
+
+public class OrdemNaoEncontrada extends Exception{
+
+
+    public OrdemNaoEncontrada(String message) {
+        super(message);
+    }
+}
