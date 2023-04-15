a111270e44283b84c43a5c44be4706566ac89ab3
Emerson-Rodrigo <102042566+Emerson-Rodrigo-Lima-Pereira@users.noreply.github.com>
2023-04-15 11:58:02 -0300

    feat: criando uma exception para TecnicoInvalido e TecnicoNaoEncontrado

---
+++ src/main/java/com/example/sistemaparagerenciamento/exceptions/tecnico/TecnicoInvalido.java
@@ -0,0 +1,9 @@
+package com.example.sistemaparagerenciamento.exceptions.tecnico;
+
+public class TecnicoInvalido extends Exception{
+
+
+    public TecnicoInvalido(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/sistemaparagerenciamento/exceptions/tecnico/TecnicoNaoEncontrado.java
@@ -0,0 +1,9 @@
+package com.example.sistemaparagerenciamento.exceptions.tecnico;
+
+public class TecnicoNaoEncontrado extends Exception{
+
+
+    public TecnicoNaoEncontrado(String message) {
+        super(message);
+    }
+}
