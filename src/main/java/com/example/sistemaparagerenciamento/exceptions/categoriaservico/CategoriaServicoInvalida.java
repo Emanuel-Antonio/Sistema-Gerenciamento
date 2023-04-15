81100c271cbef0e19e6392a2bb9991b60cbf52db
Emerson-Rodrigo <102042566+Emerson-Rodrigo-Lima-Pereira@users.noreply.github.com>
2023-04-15 12:03:20 -0300

    feat: criando uma exception para CategoriaServicoInvalida

---
+++ src/main/java/com/example/sistemaparagerenciamento/exceptions/categoriaservico/CategoriaServicoInvalida.java
@@ -0,0 +1,9 @@
+package com.example.sistemaparagerenciamento.exceptions.categoriaservico;
+
+public class CategoriaServicoInvalida extends Exception{
+
+
+    public CategoriaServicoInvalida(String message) {
+        super(message);
+    }
+}
