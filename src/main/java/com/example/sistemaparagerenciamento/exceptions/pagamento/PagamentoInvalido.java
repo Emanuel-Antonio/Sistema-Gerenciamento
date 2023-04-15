c9c2c0a63fa334f297bef57a87d305121f8d872e
Emerson-Rodrigo <102042566+Emerson-Rodrigo-Lima-Pereira@users.noreply.github.com>
2023-04-15 11:49:25 -0300

    feat: criando uma exception para PagamentoInvalido e PagamentoNaoEncontrado

---
+++ src/main/java/com/example/sistemaparagerenciamento/exceptions/pagamento/PagamentoInvalido.java
@@ -0,0 +1,9 @@
+package com.example.sistemaparagerenciamento.exceptions.pagamento;
+
+public class PagamentoInvalido extends Exception{
+
+
+    public PagamentoInvalido(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/sistemaparagerenciamento/exceptions/pagamento/PagamentoNaoEncontrado.java
@@ -0,0 +1,9 @@
+package com.example.sistemaparagerenciamento.exceptions.pagamento;
+
+public class PagamentoNaoEncontrado extends Exception{
+
+
+    public PagamentoNaoEncontrado(String message) {
+        super(message);
+    }
+}
