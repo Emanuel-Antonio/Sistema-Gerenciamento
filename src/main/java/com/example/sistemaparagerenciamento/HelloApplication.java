c01a17ddbd2468542651555607fb3139bdfd889a
Emanuel-Antonio <102038148+Emanuel-Antonio@users.noreply.github.com>
2023-05-13 16:34:44 -0300

    feat: esboço tela login

--- src/main/java/com/example/sistemaparagerenciamento/HelloApplication.java
+++ src/main/java/com/example/sistemaparagerenciamento/HelloApplication.java
@@ -2,22 +2,22 @@ package com.example.sistemaparagerenciamento;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("pagina.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
+    public static void main(String[] args) { launch(args); }
 }
--- src/main/java/com/example/sistemaparagerenciamento/HelloController.java
+++ src/main/java/com/example/sistemaparagerenciamento/HelloController.java
@@ -1,14 +1,19 @@
 package com.example.sistemaparagerenciamento;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.InputMethodEvent;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField email;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField senha;
+
+    @FXML
+    void onHelloButtonClick(ActionEvent event) {
     }
 }
